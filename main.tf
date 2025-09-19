899d5fdd120c38a678e46f7bc96dc4ea92b33f52
Gus M <amuwonge99@gmail.com>
2025-09-19 04:22:48 +0100

    new pipeline built

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "s3 bucket" {
-  bucket = "gus-versus-the-world"
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "gus-versus-the-world1"
+   force_destroy = true
 }
